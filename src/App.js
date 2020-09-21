47eafa8f1c9575ef9757eddb49e82cb0688f7ed0
Tornike <tornike.lomidze94@gmail.com>
2020-09-21 12:31:10 +0400

    weather app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TodayWeather from "./component/todayWeather";
+
+function App() {
+  return (
+    <div className="App">
+
+      <TodayWeather/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/todayWeather/index.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+function TodayWeather() {
+    const key = "46439992e5ac096548129dc64880f293";
+    const City = "Tbilisi";
+    const Country = "Georgia";
+
+    const [weatherData, setWeatherData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [iconID, setIconID] = useState(null);
+
+    const weatherAPI = () => {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?q=${City},${Country}&APPID=${key}&units=metric`)
+            .then((data) => {
+                console.log(data);
+
+                setIsLoading(false);
+                setWeatherData(data);
+                setIconID(data.data.weather[0].icon);
+
+            })
+            .catch((e) => console.log(e))
+            .finally(() => setIsLoading(true));
+    };
+
+    useEffect(() => {
+        weatherAPI();
+    }, []);
+
+    let weatherIcon = `http://openweathermap.org/img/wn/${iconID}@2x.png`;
+
+    return (
+        <div className="weather_row">
+            {isLoading ? (
+                <div className="weather_item">
+                    <img src={weatherIcon} />
+                    <span className="description">
+                        {weatherData.data.weather[0].description}
+                    </span>
+                    <h1 className="temp">
+                        {weatherData.data.main.temp}
+                        <span className="celsus-icon">&#9900;</span>
+                    </h1>
+                    <h3 className="title">
+                        <span>{weatherData.data.name}</span>,
+            <span className="abrev"> {weatherData.data.sys.country}</span>
+                    </h3>
+                </div>
+            ) : (
+                    <FontAwesomeIcon className="spiner" icon={faSpinner} />
+                )}
+        </div>
+    );
+}
+export default TodayWeather;
